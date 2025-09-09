fca8b39471f973fa324f95ef856add2d230a48b6
kamalessh01 <111232185+kamalessh01@users.noreply.github.com>
2025-09-08 20:05:59 -0400

    Update Task1_Python_script.py

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -4,30 +4,26 @@
 import os
 import requests
 import re
+import sys 
 # Code here - Import BeautifulSoup library
-
+from bs4 import BeautifulSoup
 # Code ends here
 
 # function to get the html source text of the medium article
-def get_page():
-	global url
-	
-	# Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
-	
-	# Code ends here
-	
-	# handling possible error
-	if not re.match(r'https?://medium.com/',url):
-		print('Please enter a valid website, or make sure it is a medium article')
-		sys.exit(1)
+def get_page(url):
+    if not re.match(r'https?://medium.com/', url):
+        print('Please enter a valid website, or make sure it is a Medium article')
+        sys.exit(1)
 
-	# Code here - Call get method in requests object, pass url and collect it in res
-	
-	# Code ends here
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the page: {e}")
+        sys.exit(1)
 
-	res.raise_for_status()
-	soup = BeautifulSoup(res.text, 'html.parser')
-	return soup
+    soup = BeautifulSoup(res.text, 'html.parser')
+    return soup   # FIX: removed unreachable duplicate code after this
 
 # function to remove all the html tags and replace some with specific strings
 def clean(text):
@@ -35,36 +31,38 @@ def clean(text):
     rep = dict((re.escape(k), v) for k, v in rep.items()) 
     pattern = re.compile("|".join(rep.keys()))
     text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
-    text = re.sub('\<(.*?)\>', '', text)
+    text = re.sub(r'<(.*?)>', '', text)   # FIX: raw string to avoid escape warning
     return text
 
 
 def collect_text(soup):
-	text = f'url: {url}\n\n'
-	para_text = soup.find_all('p')
-	print(f"paragraphs text = \n {para_text}")
-	for para in para_text:
-		text += f"{para.text}\n\n"
-	return text
+    text = f'url: {url}\n\n'
+    para_text = soup.find_all('p')
+    print(f"paragraphs text = \n {para_text}")
+    for para in para_text:
+        text += f"{para.text}\n\n"
+    return text
 
 # function to save file in the current directory
 def save_file(text):
-	if not os.path.exists('./scraped_articles'):
-		os.mkdir('./scraped_articles')
-	name = url.split("/")[-1]
-	print(name)
-	fname = f'scraped_articles/{name}.txt'
-	
-	# Code here - write a file using with (2 lines)
-	
+    if not os.path.exists('./scraped_articles'):
+        os.mkdir('./scraped_articles')
+    name = url.split("/")[-1]
+    print(name)
+    fname = f'scraped_articles/{name}.txt'
+    
+    # Code here - write a file using with (2 lines)
+    with open(fname, 'w', encoding='utf-8') as f:
+        f.write(text)   # FIX: corrected indentation
+    # Code ends here
 
-	# Code ends here
-
-	print(f'File saved in directory {fname}')
+    print(f'File saved in directory {fname}')
 
 
 if __name__ == '__main__':
-	text = collect_text(get_page())
-	save_file(text)
-	# Instructions to Run this python code
-	# Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
+    url = input("Enter url of a medium article: ").strip()   # FIX: url input belongs here
+    soup = get_page(url)
+    text = collect_text(soup)
+    save_file(text)
+    # Instructions to Run this python code
+    # Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
